d4c1c1726f56b611cd343274e23b988f16853ebb
zjxstar <zjx1459871421@163.com>
2020-10-29 15:21:32 +0800

    feat:调整LoadingView的样式

--- src/components/LoadingView.js
+++ src/components/LoadingView.js
@@ -1,19 +1,15 @@
 import React, { PureComponent } from 'react';
-import { View, StyleSheet } from 'react-native';
-import { Image } from 'react-native-elements';
+import { View, StyleSheet, ActivityIndicator } from 'react-native';
 import Color from '../styles/color';
 import { getRealDP as dp } from '../utils/screenUtil';
 
 export default class LoadingView extends PureComponent {
 
-
     render () {
 
         return (
             <View style={styles.container}>
-                <Image
-                    source={require('../images/ic_launcher.png')}
-                    style={{ width: dp(200), height: dp(200) }} />
+                <ActivityIndicator size={dp(80)} color={Color.THEME} />
             </View>
         )
     }
